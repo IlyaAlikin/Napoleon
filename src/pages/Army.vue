3dd0b33a8726f771a958892cfe1e45392866c2b6
Ilya <iljuha.alikin@gmail.com>
2024-06-02 23:05:03 +0500

    added type of army

--- src/pages/Army.vue
+++ src/pages/Army.vue
@@ -16,10 +16,10 @@
     <div class="section__container">
       <div class="section__block block-brief">
         <div class="section__text">
-          <!-- <div class="biography__top">
-            <h2 class="section__title white">Становление армии Наполеона</h2>
-          </div> -->
           <div class="section__wrapper army-wrapper">
+            <div class="biography__top">
+              <h2 class="section__title white">Становление "Grande Armée"</h2>
+            </div>
             <div class="block-section">
               <h3 class="block-section__title white">
                 Ранний период и влияние революции (1789-1796):
@@ -151,6 +151,138 @@
         </div>
       </div>
     </div>
+    <div class="section__container">
+      <div class="section__block block-brief">
+        <!-- <div class="section__text">
+          <div class="biography__top army">
+            <h2 class="section__title white">Роды войск "Grande Armée"</h2>
+          </div>
+          <div class="army__wrapper">
+            <h4 class="block-army__title white">
+              Армия Наполеона, известная как "Grande Armée", обладала передовой
+              для своего времени организацией и включала в себя следующие
+              основные рода войск:
+            </h4>
+            <div class="army__block block-army">
+              <div class="army__block__name">1. Пехота:</div>
+              <ul class="army-block__text">
+                <li class="army-block__li">
+                  Линейная пехота: составляла основу армии и использовалась для
+                  фронтальных атак и обороны.
+                </li>
+                <li class="army-block__li">
+                  Легкая пехота: более мобильная, вооруженная ружьями с нарезным
+                  стволом, использовалась для разведки, засад и ведения
+                  стрельбы.
+                </li>
+                <li class="army-block__li">
+                  Гренадеры: элитные подразделения линейной пехоты, отличавшиеся
+                  высоким ростом, выправкой и использовавшиеся для штурма
+                  укреплений.
+                </li>
+                <li class="army-block__li">
+                  Вольтижеры: элитные части легкой пехоты, отличавшиеся
+                  меткостью и использовавшиеся для разведки и перестрелок.
+                </li>
+              </ul>
+              <div class="block-army__img">
+                <img src="" alt="" />
+              </div>
+            </div>
+          </div>
+        </div> -->
+        <div class="section__text">
+          <div class="section__wrapper army-wrapper">
+            <div class="biography__top">
+              <h2 class="section__title white">Состав "Grande Armée"</h2>
+            </div>
+            <div class="block-section">
+              <h3 class="block-section__title white">Пехота:</h3>
+              <div class="block-section__wrapper">
+                <div class="block-section__text white">
+                  <p class="section__paragraph">
+                    Линейная пехота: составляла основу армии и использовалась
+                    для фронтальных атак и обороны.
+                  </p>
+                  <p class="section__paragraph">
+                    Легкая пехота: более мобильная, вооруженная ружьями с
+                    нарезным стволом, использовалась для разведки, засад и
+                    ведения стрельбы.
+                  </p>
+                  <p class="section__paragraph">
+                    Гренадеры: элитные подразделения линейной пехоты,
+                    отличавшиеся высоким ростом, выправкой и использовавшиеся
+                    для штурма укреплений.
+                  </p>
+                  <p class="section__paragraph">
+                    Вольтижеры: элитные части легкой пехоты, отличавшиеся
+                    меткостью и использовавшиеся для разведки и перестрелок.
+                  </p>
+                </div>
+                <div class="section__slider block-section__img">
+                  <img src="/img/Infantry.jpg" alt="" />
+                  <p class="img-section-descripton">
+                    Французская линейная пехота: <br />
+                    гренадер и вольтижёр, 1808 год
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="block-section">
+              <h3 class="block-section__title white">Кавалерия:</h3>
+              <div class="block-section__wrapper">
+                <div class="block-section__text white">
+                  <p class="section__paragraph">
+                    Тяжелая кавалерия: кирасиры и кавалергарды, облаченные в
+                    тяжелые доспехи, использовались для мощных прорывных атак.
+                  </p>
+                  <p class="section__paragraph">
+                    Линейная (средняя) кавалерия: драгуны и карабинеры, более
+                    маневренная, использовалась для атак на фланги и
+                    преследования противника.
+                  </p>
+                  <p class="section__paragraph">
+                    Легкая кавалерия: гусары, уланы и конные егеря, наиболее
+                    мобильная, предназначалась для разведки, рейдов и связи.
+                  </p>
+                </div>
+                <div class="section__img block-section__img">
+                  <img src="/img/Kirrasir.jpg" alt="" />
+                  <p class="img-section-descripton">Киррасир</p>
+                </div>
+              </div>
+            </div>
+            <div class="block-section">
+              <h3 class="block-section__title white">Артиллерия:</h3>
+              <div class="block-section__wrapper">
+                <div class="block-section__text white">
+                  <p class="section__paragraph">
+                    Полевая артиллерия: основная ударная сила армии,
+                    использовавшая различные типы пушек для поддержки пехоты и
+                    кавалерии.
+                  </p>
+                  <p class="section__paragraph">
+                    Конная артиллерия: мобильная артиллерия, способная быстро
+                    перемещаться по полю боя.
+                  </p>
+                  <p class="section__paragraph">
+                    Инженерные войска: отвечали за строительство укреплений,
+                    мостов, дорог и проведение осадных работ.
+                  </p>
+                </div>
+                <div class="section__img block-section__img">
+                  <img src="/img/Artillery.jpg" alt="" />
+                  <p class="img-section-descripton">
+                    Полевая артиллерия: <br />
+                    основная ударная сила армии
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 <script setup></script>
@@ -159,5 +291,9 @@
   max-width: 350px;
   height: 500px;
   object-fit: cover;
+}
+
+.army__wrapper {
+  max-width: 900px;
 }
 </style>
